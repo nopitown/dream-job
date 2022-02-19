5ca957b619eef7fca260e59eccf230c10456df67
Elizabeth Manrique <hey@nopitown.com>
2022-02-19 17:52:44 -0500

    feat: add Google auth

--- app/routes/job-applications.tsx
+++ app/routes/job-applications.tsx
@@ -1,10 +1,33 @@
 import { Outlet } from "remix";
+import type { ActionFunction, LoaderFunction } from "remix";
+import { Form, json, useLoaderData } from "remix";
+import type { GoogleProfile } from "remix-auth-google";
+import { auth } from "~/auth.server";
+
+type LoaderData = { profile: GoogleProfile };
+
+export const action: ActionFunction = async ({ request }) => {
+  await auth.logout(request, { redirectTo: "/" });
+};
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const { profile } = await auth.isAuthenticated(request, {
+    failureRedirect: "/",
+  });
+
+  return json<LoaderData>({ profile });
+};
 
 export default function JobApplicationsScreen() {
+  useLoaderData<LoaderData>();
+
   return (
     <>
       <header>
         <h1>Job Applications</h1>
+        <Form method="post">
+          <button>Log Out</button>
+        </Form>
       </header>
       <main>
         <Outlet />
