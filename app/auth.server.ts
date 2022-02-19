90cd7fca760997a0f12c3d5ab3f3ed1d65498d52
Elizabeth Manrique <hey@nopitown.com>
2022-02-19 18:22:25 -0500

    feat: add google auth (#4)
    * feat: add Google auth
    
    * fix: ignore .env from tracking
    
    * fix: remove default db name
    
    * feat: add prettierrc
    
    * chore: add COOKIES_SECRET to sign cookies
    
    * feat: read cookies secret from env variables
    
    * refactor: move auth routes to its own folder
    
    * fix: add missing new line at end of file

---
+++ app/auth.server.ts
@@ -0,0 +1,52 @@
+import { createCookieSessionStorage } from "remix";
+import { Authenticator } from "remix-auth";
+import type { GoogleExtraParams, GoogleProfile } from "remix-auth-google";
+import { GoogleStrategy } from "remix-auth-google";
+
+if (!process.env.GOOGLE_CLIENT_ID) {
+  throw new Error("GOOGLE_CLIENT_ID is required");
+}
+
+if (!process.env.GOOGLE_CLIENT_SECRET) {
+  throw new Error("GOOGLE_CLIENT_SECRET is required");
+}
+
+if (!process.env.BASE_URL) {
+  throw new Error("BASE_URL is required");
+}
+
+if (!process.env.COOKIES_SECRET) {
+  throw new Error("COOKIES SECRETS is required");
+}
+
+const BASE_URL = process.env.BASE_URL;
+
+export const sessionStorage = createCookieSessionStorage({
+  cookie: {
+    name: "__session",
+    httpOnly: true,
+    path: "/",
+    sameSite: "lax",
+    secrets: [process.env.COOKIES_SECRET],
+    secure: process.env.NODE_ENV === "production",
+  },
+});
+
+export const auth = new Authenticator<{
+  profile: GoogleProfile;
+  accessToken: string;
+  extraParams: GoogleExtraParams;
+}>(sessionStorage);
+
+auth.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      callbackURL: new URL("/auth/google/callback", BASE_URL).toString(),
+    },
+    async ({ profile, accessToken, extraParams }) => {
+      return { profile, accessToken, extraParams };
+    }
+  )
+);
